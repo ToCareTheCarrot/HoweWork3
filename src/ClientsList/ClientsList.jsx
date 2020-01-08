89667c0de3257738e87026a8bb94a30f162ead10
Your Name <you@example.com>
2020-01-08 20:01:01 +0300

    Сделал 1 и 2 пункты ДЗ

--- src/ClientsList/ClientsList.jsx
+++ src/ClientsList/ClientsList.jsx
@@ -2,38 +2,60 @@ import React from "react";
 import { AddForm } from "./AddForm";
 
 function addClient (
-    clients,
-    clientToAdd
+  clients,
+  clientToAdd
 ){
-    return [...clients, clientToAdd];
+  return [...clients, clientToAdd];
 }
 
 export class ClientsList extends React.Component {
-    state = {
-        clients: [
-            {
-                id : 1,
-                name: "Alex",
-                telephoneNumber: "+375296445252",
-            }
-        ],
-    };
+  state = {
+    clients: [
+      {
+        id : "",
+        name: "",
+        phone: "",
+      }
+    ],
+  };
 
-    render(){
-        return(
-            <>
-                <AddForm/>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>id</th>
-                            <th>Name</th>
-                            <th>telephone</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                </table>
-            </>
-        );
-    }
+  nextId = 1;
+
+  render(){
+    return(
+      <>
+        <AddForm
+          onSave={(name,phone) => {
+            const client={
+              id: this.nextId,
+              name: name,
+              phone: phone,
+            };
+            this.setState({
+              clients: addClient(this.state.clients, client)
+            });
+            this.nextId++;
+          }
+        }/>
+        <table>
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>Name</th>
+              <th>Phone</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.clients.map(client => (
+              <tr key={client.id}>
+                <td>{client.id}</td>
+                <td>{client.name}</td>
+                <td>{client.phone}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </>
+    );
+  }
 }
